8accb5eac560847c98f7dff1f3db18d300d36aaa
Ronald Brachetti <brachial.software@gmail.com>
2023-10-03 18:46:35 +0200

    basic support for constants

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -fdiagnostics-color=always -Wall -g src/main.c src/debug.c src/chunk.c src/memory.c -o target/main
+gcc -fdiagnostics-color=always -Wall -g src/main.c src/debug.c src/chunk.c src/memory.c src/value.c -o target/main
