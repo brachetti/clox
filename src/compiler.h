c0ab30d736eaad8918c03cfaf06ef3798ddae034
Ronald Brachetti <brachial.software@gmail.com>
2023-10-04 21:01:59 +0200

    basis for parsing expressions

--- src/compiler.h
+++ src/compiler.h
@@ -1,6 +1,10 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char* source);
+#include <stdlib.h>
+
+#include "chunk.h"
+
+bool compile(const char* source, Chunk* chunk);
 
 #endif
